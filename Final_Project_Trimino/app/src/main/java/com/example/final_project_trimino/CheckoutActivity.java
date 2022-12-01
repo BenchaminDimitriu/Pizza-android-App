e83ea2a9eb3282b6b89afdaf2d961ea8c2158867
EaEmmy <82058275+EaEmmy@users.noreply.github.com>
2022-12-01 12:17:08 -0500

    i forgot checkout :I
    +added checkout xml & activity

---
+++ Final_Project_Trimino/app/src/main/java/com/example/final_project_trimino/CheckoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.final_project_trimino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
+}
